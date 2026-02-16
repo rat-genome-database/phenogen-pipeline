38b2d22b1dd15cb9ee23a23c70076ddaf07c8055
tutajm <mtutaj@mcw.edu>
2026-02-16 16:45:35 -0600

    updated dependencies

--- src/main/dist/run.sh
+++ src/main/dist/run.sh
@@ -8,7 +8,7 @@ APPDIR=/home/rgddata/pipelines/$APPNAME
 SERVER=`hostname -s | tr '[a-z]' '[A-Z]'`
 EMAIL_LIST=mtutaj@mcw.edu
 if [ "$SERVER" = "REED" ]; then
-  EMAIL_LIST=rgd.devops@mcw.edu
+  EMAIL_LIST=mtutaj@mcw.edu
 fi
 
 cd $APPDIR
